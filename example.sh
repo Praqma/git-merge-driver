3ac75840aebadfcadd8a987da77d5dcbeacf44c0
Alexei Kharchev <alexei_sylver1@mail.ru>
2024-02-04 16:15:31 +0800

    Added ours/theirs policies

--- example.sh
+++ example.sh
@@ -15,16 +15,22 @@ git branch -D demo-branch-2
 
 # Create 'my-file.mrg' on branch 1
 git checkout -b demo-branch-1
-echo "created on: demo-branch-1" > my-file.mrg
-git add my-file.mrg
-git commit -m"demo-branch-1: added my-file.mrg"
+[[ -d lib ]] || mkdir lib
+echo "Description 1 in README.md" > README.md
+echo "created on: demo-branch-1" > lib/my-file.mrg
+git add README.md
+git add lib/my-file.mrg
+git commit -m "demo-branch-1: added my-file.mrg"
 
 # Create 'my-file.mrg' on branch 2
 git checkout master
 git checkout -b demo-branch-2
-echo "created on: demo-branch-2" > my-file.mrg
-git add my-file.mrg
-git commit -m"demo-branch-2: added my-file.mrg"
+[[ -d lib ]] || mkdir lib
+echo "Description 2 in README.md" > README.md
+echo "created on: demo-branch-2" > lib/my-file.mrg
+git add README.md
+git add lib/my-file.mrg
+git commit -m "demo-branch-2: added my-file.mrg"
 
 # Merge the two branches, causing a conflict
-git merge -m"Merged in demo-branch-1" demo-branch-1
+git merge -m "Merged in demo-branch-1" demo-branch-1
---
+++ git_drivers/theirs.sh
@@ -0,0 +1,43 @@
+printf "checking ...\n"
+diff $2 $3 && exit 0
+
+printf "
+#######################
+# Merge driver called #
+#######################\n"
+
+cp -f $3 $2
+echo "file was overwrited from another branch!"
+
+exit 0
+
+
+
+
+#printf "ancestor: $1
+#---------\n"
+#cat $1
+#printf "\n"
+#
+#printf "current branch: $2
+#---------\n"
+#cat $2
+#printf "\n"
+#
+#printf "another branch: $3
+#---------\n"
+#cat $3
+#printf "\n"
+#
+#printf "Resolving conflict ...\n"
+#sleep 0.5
+#cat $3 > $2
+##echo "This is the merge result" > $2
+#echo "Conflict resolved!"
+#
+#printf "resolved: $2\n
+#---------\n"
+#cat $2
+#printf "\n"
+#
+#exit 0
